5b24e7b647b0f9079598826964b01a547eaf6af1
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2023-11-17 21:58:01 +0300

    toBe or notToBe

---
+++ leetCode/toBe or notToBe.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+   
+    return{
+        toBe(value){
+            if(value===val){
+                return true;
+            }else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe(value){
+             if(value!==val){
+                 return true;
+             }else{
+                 throw new Error("Equal");
+             }
+        }
+    }
+  };
+  
+  
+  
+  
+  
