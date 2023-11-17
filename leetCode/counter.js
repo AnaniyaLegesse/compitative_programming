e238e76d730901d439c7be65e959cf0d087454d4
AnaniyaLegesse <ananiya.legesse1@gmail.com>
2023-11-17 11:34:38 +0300

    counter js

---
+++ leetCode/counter.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+
+    return function() {
+        
+        return n++;
+      
+    };
+    
+};
+
